e1642c8554e7cf97fd60a10e971e4dcb87c997f3
dmiles <dmiles@monsooncommerce.com>
2015-06-30 10:33:39 -0700

    Added tests for proxy

--- proxy_test.go
+++ proxy_test.go
@@ -40,5 +40,42 @@ func TestProxy(t *testing.T) {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(string(bodyBytes)).To(Equal("these are the bytes from proxied service."))
 		})
+		g.It("should proxy headers from remote", func() {
+			proxyToThis := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("X-my-custom-header", "my custom header value")
+				w.WriteHeader(200)
+				w.Write([]byte("these are the bytes from proxied service."))
+			}))
+			logHandler := log.LogHandlerImpl{logger}
+
+			server := httptest.NewServer(MakeProxiedHandler(proxyToThis.URL, &logHandler))
+			req, err := http.NewRequest("GET", server.URL, nil)
+			Expect(err).NotTo(HaveOccurred())
+			client := http.Client{}
+			resp, err := client.Do(req)
+			Expect(err).NotTo(HaveOccurred())
+			defer resp.Body.Close()
+			myCustomHeader := resp.Header.Get("X-my-custom-header")
+			Expect(myCustomHeader).To(Equal("my custom header value"))
+		})
+		g.It("should proxy headers to remote", func() {
+			recievedHeaderValue := ""
+			proxyToThis := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				recievedHeaderValue = r.Header.Get("X-my-custom-header")
+				w.WriteHeader(200)
+				w.Write([]byte("these are the bytes from proxied service."))
+			}))
+			logHandler := log.LogHandlerImpl{logger}
+
+			server := httptest.NewServer(MakeProxiedHandler(proxyToThis.URL, &logHandler))
+			req, err := http.NewRequest("GET", server.URL, nil)
+			req.Header.Add("X-my-custom-header", "my custom header value")
+			Expect(err).NotTo(HaveOccurred())
+			client := http.Client{}
+			resp, err := client.Do(req)
+			Expect(err).NotTo(HaveOccurred())
+			defer resp.Body.Close()
+			Expect(recievedHeaderValue).To(Equal("my custom header value"))
+		})
 	})
 }
